4f70c931915971494a7bd01e18f7c7141ff109ed
Glenn K. Lockwood <glennklockwood@gmail.com>
2024-08-10 16:14:04 -0700

    add checker for content

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# if --test is passed on the command line,
+if [ ! -d content ]; then
+    echo "No content directory. Did you remember to symlink your Obsidian vault?" >&2
+    echo "ln -s /path/to/obsidian/vault content" >&2
+    exit 1
+fi
 
 if [ "$1" == "test" -o "$1" == "--test" ]; then
     npx quartz build --serve
