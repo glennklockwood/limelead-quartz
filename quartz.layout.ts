7cf85553a659ce4932c9b4682fffc67aad2da94c
Glenn K. Lockwood <glennklockwood@gmail.com>
2024-08-11 20:45:12 -0700

    add a little more dimensionality to the graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,7 +35,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+        localGraph: {
+            depth: 2,
+        },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
