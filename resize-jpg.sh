f2a3b0140d00fb775655593067dec107529fd3c0
Glenn K. Lockwood <glockwood@microsoft.com>
2024-08-13 13:02:16 -0700

    update tool

--- resize-jpg.sh
+++ resize-jpg.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# applies convert input.jpg -resize 1280x1280\> -quality 85% output.jpg to a given jpg file
+# applies convert input.jpg -resize 1280x1280\> -quality 85% output.jpg to a
+# given image.
+
+set -e
 
 INPUT_FILE="$1"
 
@@ -13,13 +16,14 @@ fi
 # get the file extension of the input file
 FILE_EXTENSION="${INPUT_FILE##*.}"
 
-echo "Got file extension $FILE_EXTENSION"
+# use mktemp or tempfile or whatever the preferred command is to make a new
+# file with the same extension as the input file. it has to work on both
+# linux and macos
+#TMP_FILE=$(mktemp -u --suffix=".${FILE_EXTENSION}")
+TMP_FILE="$(mktemp -u).${FILE_EXTENSION}"
 
-TMP_FILE="./tmp.${FILE_EXTENSION}"
-
-set -e
 ls -l "$INPUT_FILE"
-cp "$INPUT_FILE" "$TMP_FILE"
+cp -v "$INPUT_FILE" "$TMP_FILE"
 convert "$TMP_FILE" -resize 1280x1280\> -quality 85% "$INPUT_FILE"
-rm $TMP_FILE
+rm -v $TMP_FILE
 ls -l "$INPUT_FILE"
