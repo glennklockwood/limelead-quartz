8506f67439306173f34004d8bcb27676036b0e14
Glenn K. Lockwood <glockwood@microsoft.com>
2024-08-26 11:39:30 -0700

    add back recents

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,6 +29,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.RecentNotes({
+      limit: 5,
+      showTags: false,
+    })),
   ],
   right: [
     Component.Graph({localGraph: {depth: 2}}),
