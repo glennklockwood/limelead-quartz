1d6bb8139493247ab0c78d9f4e7993768bbfae29
Glenn K. Lockwood <glennklockwood@gmail.com>
2026-01-29 13:37:30 -0800

    fixes

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ compare() {
 }
 
 build_only() {
-    npx quartz build --concurrency 8 && mv -v public/static/.htaccess public/
+    npx quartz build --concurrency 8 && cp -v quartz/static/.htaccess public/
     test -f public/index.html
 }
 
